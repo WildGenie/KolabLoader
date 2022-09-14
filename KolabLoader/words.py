aa6929273bb37d7346b5525f224c3e9e10b15a94
Sourcery AI <>
2022-09-14 20:16:56 +0000

    'Refactored by Sourcery'

--- KolabLoader/words.py
+++ KolabLoader/words.py
@@ -1,11 +1,10 @@
-fh = open('words.txt')
-fh2 = open("out.txt", "w")
-for line in fh:
-    # in python 2
-    # print line
-    # in python 3
-    if(len(line) > 6 and len(line) < 12):
-        fh2.write(line)
- 
-fh.close() 
+with open('words.txt') as fh:
+    fh2 = open("out.txt", "w")
+    for line in fh:
+        # in python 2
+        # print line
+        # in python 3
+        if(len(line) > 6 and len(line) < 12):
+            fh2.write(line)
+
 fh2.close()
